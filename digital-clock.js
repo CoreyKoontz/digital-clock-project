bc756c6ee15d4ef56d67838f21966949e6857c9f
Corey Koontz <corey.michael.koontz@gmail.com>
2020-12-21 19:30:23 -0600

    feat: update code on digital clock project

--- digital-clock.js
+++ digital-clock.js
@@ -1,5 +1,27 @@
 "use strict"
+//remember these functions when creating a employee time tracker
 
 function currentTime() {
-
+var date = new Date();
+var hour = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+hour = updateTime(hour);
+min = updateTime(min);
+sec = updateTime(sec);
+document.getElementById("clock").innertext = hour + " : " +
+    min + " : " + sec;
+var t = setTimeout(function (){currentTime()
+    }, 1000);
 }
+
+function updateTime(k) {
+    if (k < 10) {
+        return "0" + k;
+    }
+    else {
+        return k;
+    }
+}
+
+currentTime();
